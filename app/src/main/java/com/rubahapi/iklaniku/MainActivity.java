6574013c6534f6dff8738c596534c64bb3292dec
junifar <junifar@gmail.com>
2016-12-15 17:12:53 +0700

    + Sign In Page Show Signin OnClick Done + Login success -  result still in false ==> handleSignInResult:false

--- app/src/main/java/com/rubahapi/iklaniku/MainActivity.java
+++ app/src/main/java/com/rubahapi/iklaniku/MainActivity.java
@@ -6,9 +6,13 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 
@@ -18,11 +22,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private static final int RC_SIGN_IN = 9001;
 
     private GoogleApiClient googleApiClient;
+    private TextView statusTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        statusTextView = (TextView) findViewById(R.id.status);
 
         findViewById(R.id.sign_in_button).setOnClickListener(this);
 
@@ -54,5 +61,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
         Log.d(TAG, "onConnectionFailed:" + connectionResult);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN){
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        Log.d(TAG, "handleSignInResult:" + result.isSuccess());
+
+        if (result.isSuccess()){
+            GoogleSignInAccount account = result.getSignInAccount();
+            statusTextView.setText(account != null ? account.getDisplayName() : null);
+            updateUI(true);
+        }else{
+            updateUI(false);
+        }
+    }
+
+    private void updateUI(boolean signedIn) {
+        if(signedIn){
+            Toast.makeText(this,"Signed In",Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this,"Signed Out",Toast.LENGTH_SHORT).show();
+        }
     }
 }
