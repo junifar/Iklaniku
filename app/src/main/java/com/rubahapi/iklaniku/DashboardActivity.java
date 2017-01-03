4388965d0636814ed488ac8d7d758643dcc95028
junifar <junifar@gmail.com>
2017-01-03 16:26:04 +0700

    + Sign In Page Show Signin OnClick Done + Login success + Result Success + Sign Out + Retrieve Some information from google + go to Dashboard -  maps still error

--- app/src/main/java/com/rubahapi/iklaniku/DashboardActivity.java
+++ app/src/main/java/com/rubahapi/iklaniku/DashboardActivity.java
@@ -1,14 +1,25 @@
 package com.rubahapi.iklaniku;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class DashboardActivity extends AppCompatActivity{
+public class DashboardActivity extends AppCompatActivity implements View.OnClickListener{
 
     SharedPreferences sharedPreferences;
+    ImageView vehicleImage;
+    LinearLayout layoutQuest;
+    LinearLayout profileLayout;
+    Button buttonStart;
+    Button buttonStop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +29,61 @@ public class DashboardActivity extends AppCompatActivity{
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
 
         TextView textView1 = (TextView) findViewById(R.id.textView1);
-        textView1.setText(sharedPreferences.getString(getString(R.string.username),""));
+        vehicleImage = (ImageView) findViewById(R.id.image_vehicle);
+        layoutQuest = (LinearLayout) findViewById(R.id.layout_quest);
+        profileLayout = (LinearLayout) findViewById(R.id.profile_layout);
+        buttonStart = (Button) findViewById(R.id.buttonStart);
+        buttonStop = (Button) findViewById(R.id.buttonStop);
 
+        textView1.setText(sharedPreferences.getString(getString(R.string.username),""));
+        vehicleImage.setOnClickListener(this);
+        layoutQuest.setOnClickListener(this);
+        profileLayout.setOnClickListener(this);
+        buttonStart.setOnClickListener(this);
+        buttonStop.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.image_vehicle:
+                imageView_OnClick();
+                break;
+            case R.id.layout_quest:
+                layoutQuest_OnClick();
+                break;
+            case R.id.profile_layout:
+                profileLayout_OnClick();
+                break;
+            case R.id.buttonStart:
+                buttonStart_OnClick();
+                break;
+            case R.id.buttonStop:
+                buttonStop_OnClick();
+                break;
+        }
+    }
+
+    private void buttonStop_OnClick() {
+        Toast.makeText(this,"It's Works 5",Toast.LENGTH_SHORT).show();
+    }
+
+    private void buttonStart_OnClick() {
+        Toast.makeText(this,"It's Works 4",Toast.LENGTH_SHORT).show();
+    }
+
+    private void profileLayout_OnClick() {
+        Intent intent = new Intent(DashboardActivity.this, ProfileActivity.class);
+        this.startActivity(intent);
+    }
+
+    private void layoutQuest_OnClick() {
+        Intent intent = new Intent(DashboardActivity.this, QuestSelectActivity.class);
+        this.startActivity(intent);
+    }
+
+    private void imageView_OnClick() {
+        Intent intent = new Intent(DashboardActivity.this, VehicleSelectActivity.class);
+        this.startActivity(intent);
+    }
 }
---
+++ app/src/main/java/com/rubahapi/iklaniku/QuestSelectActivity.java
@@ -0,0 +1,13 @@
+package com.rubahapi.iklaniku;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QuestSelectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quest_select);
+    }
+}
