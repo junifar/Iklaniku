920d51e203028dc76d6e8eb36b9aaa9da9f7f0f3
junifar <junifar@gmail.com>
2017-01-11 10:48:47 +0700

    -  maps still error -  adding field in profile activity -  adding datepicker for birthday -  adding image circle -  adding SQLiteVehicle Table

--- app/src/main/java/com/rubahapi/iklaniku/data/SpotIklanContract.java
+++ app/src/main/java/com/rubahapi/iklaniku/data/SpotIklanContract.java
@@ -1,5 +1,8 @@
 package com.rubahapi.iklaniku.data;
 
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -9,12 +12,16 @@ import android.provider.BaseColumns;
 class SpotIklanContract {
 
     static final String CONTENT_AUTHORITY = "com.rubahapi.iklaniku";
+    private static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
 
     static final String PATH_SPOT_IKLAN = "spotiklan";
 
     static final class VehicleEntry implements BaseColumns{
 
         static final String PATH_VEHICLE = "vehicle";
+        static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_VEHICLE).build();
+
+        static final String CONTENT_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH_SPOT_IKLAN;
 
         static final String TABLE_NAME = "VEHICLES";
         static final String COLUMN_ID = "ID";
@@ -22,5 +29,9 @@ class SpotIklanContract {
         static final String COLUMN_PLAT_NUMBER = "PLAT_NUMBER";
         static final String COLUMN_IMAGE_PATH = "IMAGE_PATH";
         static final String COLUMN_VEHICLE_TYPE = "VEHICLE_TYPE";
+
+        static Uri buildVehicleUri(long id){
+            return ContentUris.withAppendedId(CONTENT_URI, id);
+        }
     }
 }
--- app/src/main/java/com/rubahapi/iklaniku/data/SpotIklanProvider.java
+++ app/src/main/java/com/rubahapi/iklaniku/data/SpotIklanProvider.java
@@ -4,6 +4,8 @@ import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -41,14 +43,14 @@ public class SpotIklanProvider extends ContentProvider {
 
         switch (sUriMatcher.match(uri)){
             case VEHICLE: {
-                retCursor = getWeather(uri, projection, selection, selectionArgs, sortOrder);
+                retCursor = getVehicle(uri, projection, selection, selectionArgs, sortOrder);
                 break;
             }
         }
         return retCursor;
     }
 
-    private Cursor getWeather(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+    private Cursor getVehicle(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
         Cursor retCursor = spotIklanDbHelper.getReadableDatabase().query(
                 SpotIklanContract.VehicleEntry.TABLE_NAME,
                 projection,
@@ -66,22 +68,106 @@ public class SpotIklanProvider extends ContentProvider {
     @Nullable
     @Override
     public String getType(@NonNull Uri uri) {
-        return null;
+
+        final int match = sUriMatcher.match(uri);
+
+        switch (match){
+            case  VEHICLE:
+                return SpotIklanContract.VehicleEntry.CONTENT_TYPE;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
     }
 
     @Nullable
     @Override
     public Uri insert(@NonNull Uri uri, ContentValues contentValues) {
-        return null;
+        final SQLiteDatabase db = spotIklanDbHelper.getWritableDatabase();
+        final int match = sUriMatcher.match(uri);
+        Uri returnUri;
+
+        switch (match){
+            case VEHICLE:{
+                long _id = db.insert(SpotIklanContract.VehicleEntry.TABLE_NAME, null, contentValues);
+                if(_id > 0)
+                    returnUri = SpotIklanContract.VehicleEntry.buildVehicleUri(_id);
+                else
+                    throw new SQLException("Failed to insert row into " + uri);
+                break;
+            }
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+        return returnUri;
     }
 
     @Override
-    public int delete(@NonNull Uri uri, String s, String[] strings) {
-        return 0;
+    public int delete(@NonNull Uri uri, String selection, String[] selectionArgs) {
+        final SQLiteDatabase db = spotIklanDbHelper.getWritableDatabase();
+        final int match = sUriMatcher.match(uri);
+        int rowsDeleted;
+        if (null == selection) selection = "1";
+        switch (match){
+            case  VEHICLE:
+                rowsDeleted = db.delete(SpotIklanContract.VehicleEntry.TABLE_NAME, selection, selectionArgs);
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: " + uri);
+        }
+        if(rowsDeleted != 0){
+            if (getContext() != null) getContext().getContentResolver().notifyChange(uri, null);
+        }
+        return rowsDeleted;
     }
 
     @Override
-    public int update(@NonNull Uri uri, ContentValues contentValues, String s, String[] strings) {
-        return 0;
+    public int update(@NonNull Uri uri, ContentValues contentValues, String selection, String[] selectionArgs) {
+        final SQLiteDatabase db = spotIklanDbHelper.getWritableDatabase();
+        final int match = sUriMatcher.match(uri);
+        int rowsUpdated;
+
+        switch (match){
+            case VEHICLE:
+                rowsUpdated = db.update(SpotIklanContract.VehicleEntry.TABLE_NAME, contentValues, selection, selectionArgs);
+                break;
+            default:
+                throw new UnsupportedOperationException("Unknown uri: "+ uri);
+        }
+        if (rowsUpdated != 0){
+            if (getContext() != null) getContext().getContentResolver().notifyChange(uri, null);
+        }
+        return rowsUpdated;
+    }
+
+    @Override
+    public int bulkInsert(@NonNull Uri uri, @NonNull ContentValues[] values) {
+        final SQLiteDatabase db = spotIklanDbHelper.getWritableDatabase();
+        final int match = sUriMatcher.match(uri);
+        switch (match){
+            case VEHICLE:
+                db.beginTransaction();
+                int returnCount = 0;
+                try {
+                   for (ContentValues value : values){
+                       long _id = db.insert(SpotIklanContract.VehicleEntry.TABLE_NAME, null, value);
+                       if(_id != -1){
+                           returnCount++;
+                       }
+                       db.setTransactionSuccessful();
+                   }
+                }finally {
+                    db.endTransaction();
+                }
+                if(getContext() != null) getContext().getContentResolver().notifyChange(uri, null);
+                return returnCount;
+            default:
+                return super.bulkInsert(uri, values);
+        }
+    }
+
+    @Override
+    public void shutdown() {
+        spotIklanDbHelper.close();
+        super.shutdown();
     }
 }
