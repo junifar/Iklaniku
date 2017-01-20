e748fd98257e9ee5a17b03d8e7069519da365bff
junifar <junifar@gmail.com>
2017-01-20 17:35:27 +0700

    Save Information when first login

--- app/src/main/java/com/rubahapi/iklaniku/ProfileActivity.java
+++ app/src/main/java/com/rubahapi/iklaniku/ProfileActivity.java
@@ -1,6 +1,13 @@
 package com.rubahapi.iklaniku;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.app.ProgressDialog;
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.text.InputType;
@@ -8,16 +15,31 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.rubahapi.iklaniku.listener.OnDatePickerClickListener;
+import com.rubahapi.iklaniku.pojo.Driver;
+import com.rubahapi.iklaniku.service.DriverApiService;
 
 import java.util.Calendar;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 import static com.rubahapi.iklaniku.R.id.birthday_edit;
+import static com.rubahapi.iklaniku.config.ServerURL.BASE_API_URL;
 
 public class ProfileActivity extends AppCompatActivity implements View.OnClickListener, OnDatePickerClickListener {
 
     private TextView birthdayTextView;
+    private ProgressDialog mProgressDialog;
+    SharedPreferences sharedPreferences;
+    private View profileLayout;
+    private View progressView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +48,39 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
 
         birthdayTextView = (TextView) findViewById(R.id.birthday_edit);
         birthdayTextView.setOnClickListener(this);
-
         birthdayTextView.setInputType(InputType.TYPE_NULL);
+
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        profileLayout = findViewById(R.id.profile_layout);
+        progressView = findViewById(R.id.profile_progress);
+
+//        showProgress(true);
+        getProfileToServer(sharedPreferences.getString(getString(R.string.google_id),""),sharedPreferences.getString(getString(R.string.username),""));
+    }
+
+    private void getProfileToServer(String gid, String name){
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_API_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        DriverApiService driverApiService = retrofit.create(DriverApiService.class);
+
+        Call<List<Driver>> result = driverApiService.getDriverProfile(gid, name);
+
+        result.enqueue(new Callback<List<Driver>>() {
+            @Override
+            public void onResponse(Call<List<Driver>> call, Response<List<Driver>> response) {
+                Toast.makeText(ProfileActivity.this, "Success", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<List<Driver>> call, Throwable t) {
+                t.printStackTrace();
+            }
+        });
     }
 
     @Override
@@ -99,5 +152,63 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
     @Override
     public void onDatePickerClickListener(int year, int month, int date) {
         birthdayTextView.setText(date + "-" + month + "-" + year);
+    }
+
+    private void showProgressDialog() {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setMessage("Loading ...");
+            mProgressDialog.setIndeterminate(true);
+            mProgressDialog.setCancelable(false);
+            mProgressDialog.setCanceledOnTouchOutside(false);
+        }
+        mProgressDialog.show();
+    }
+
+    private void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.hide();
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private void showProgress(final boolean show) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2){
+
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+            profileLayout.setVisibility(show ? View.GONE : View.VISIBLE);
+            profileLayout.animate().setDuration(shortAnimTime).alpha(show ? 0 : 1).setListener(
+                    new Animator.AnimatorListener() {
+                        @Override
+                        public void onAnimationStart(Animator animator) {}
+
+                        @Override
+                        public void onAnimationEnd(Animator animator) {
+                            profileLayout.setVisibility(show ? View.GONE : View.VISIBLE);
+                        }
+
+                        @Override
+                        public void onAnimationCancel(Animator animator) {}
+
+                        @Override
+                        public void onAnimationRepeat(Animator animator) {}
+                    });
+
+            progressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            progressView.animate().setDuration(shortAnimTime).alpha(show ? 1 : 0).setListener(
+                    new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            progressView.setVisibility(show ? View.VISIBLE : View.GONE);
+                        }
+                    });
+        }else {
+            progressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            profileLayout.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
     }
 }
